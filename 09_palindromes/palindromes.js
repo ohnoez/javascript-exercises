2f47a3754abbe5e845674325554c883a282a5ee8
username <email@email.com>
2022-07-15 13:27:19 +0200

    finished palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
+const palindromes = function (palindrome) {
+    const alphanumeric = palindrome.toUpperCase().replace(/[^a-z0-9]/gi, '');
+    const reversed = palindrome.toUpperCase().replace(/[^a-z0-9]/gi, '').split("").reverse().join("");
 
+    if (alphanumeric === reversed){
+        return true;
+    }
+    return false;
 };
+
+console.log(palindromes('racecar'));
 
 // Do not edit below this line
 module.exports = palindromes;
